21f293fac2f196fdc7187c2609773b01341a6ad2
zachweix <zachweix@users.noreply.github.com>
2019-01-03 14:01:38 -0500

    Update Moadim.php

--- src/Moadim/Moadim.php
+++ src/Moadim/Moadim.php
@@ -10,4 +10,53 @@ trait Moadim
     use CholHamoed;
     use RoshChodesh;
     use AseresYimeiTeshuva;
+    
+    public static $holidays = [
+        "Rosh Hashana"         => "ראש השנה",
+        "Yom Kippur"           => "יום כיפור",
+        "Sukkos"               => "סוכות",
+        "Shmini Atzeres"       => "שמיני עצרת",
+        "Simchas Torah"        => "שמחת תורה",
+        "Rosh Chodesh Chanuka" => "ראש חודש חנוכה",
+        "Chanuka"              => "חנוכה",
+        "Tu Bishvat"           => "ט״ו בשבט",
+        "Purim Kattan"         => "פורים קטן",
+        "Purim"                => "פורים",
+        "Shushan Purim"        => "שושן פורים",
+        "Pesach"               => "פסח",
+        "Pesach Sheni"         => "פסח שני",
+        "Shavuos"              => "שבועות",
+
+        //FAST DAYS
+        "Tzom Gedaliah"        => "צום גדליה",
+        "Asara Biteives"       => "עשרה בטבת",
+        "Taanis Esther"        => "תענית אסתר",
+        "Shiva Asar Bitamuz"   => "שבעה עשר בתמוז",
+        "Tisha Bav"            => "תשעה באב",
+
+        //OTHER
+        "Rosh Chodesh"         => "ראש חודש",
+        "Aseres Yimei Teshuva" => "עשרת ימי תשובה",
+    ];
+    
+    public function getHoliday($language = 'English') {
+        foreach (self::$holidays as $english => $hebrew) {
+            try {
+                if ($this->{"is" . studly_case($english)}()) {
+                    switch ($language) {
+                        case 'English':
+                            return $english;
+                        case 'Hebrew':
+                            return $hebrew;
+                        default:
+                            return false;
+                    }
+                    
+                }
+            } catch (\Exception $e) {
+                // isPurimKattan will fail in non-leap years, so we can just ignore it
+            }
+        }
+        return false;
+    }
 }
