296c0b8bb9ca9a7464c3468d6db2cfaf9c9f226c
tam5 <arimiller92@gmail.com>
2019-04-28 19:42:01 -0400

    more style fixes

--- tests/Helpers/holidaysTest.php
+++ tests/Helpers/holidaysTest.php
@@ -2,14 +2,12 @@
 
 namespace Test\Helpers;
 
-use Zman\Exceptions\InvalidDateException;
-
 class holidaysTest extends \PHPUnit\Framework\TestCase
 {
     /** @test */
     public function it_loads_the_parshios_dictionary()
     {
-        require __DIR__ . '../../../src/Helpers/holidays.php';
+        require __DIR__.'../../../src/Helpers/holidays.php';
 
         foreach (HOLIDAYS as $holiday) {
             $this->assertArrayHasKey('english', $holiday);
--- tests/Helpers/parshiosTest.php
+++ tests/Helpers/parshiosTest.php
@@ -2,14 +2,12 @@
 
 namespace Test\Helpers;
 
-use Zman\Exceptions\InvalidDateException;
-
 class parshiosTest extends \PHPUnit\Framework\TestCase
 {
     /** @test */
     public function it_loads_the_parshios_dictionary()
     {
-        require __DIR__ . '../../../src/Helpers/parshios.php';
+        require __DIR__.'../../../src/Helpers/parshios.php';
 
         foreach (PARSHIOS as $parsha) {
             $this->assertArrayHasKey('english', $parsha);
