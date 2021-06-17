d9330468174b4b3a7e6a6da10ae606c47aa7f088
Zachary Weixelbaum <zachweix@gmail.com>
2021-06-17 09:46:13 -0400

    fix omer tests

--- tests/Moadim/OmerTest.php
+++ tests/Moadim/OmerTest.php
@@ -9,11 +9,11 @@ class OmerTest extends \PHPUnit\Framework\TestCase
     /** @test */
     public function checks_omer_count()
     {
-        $this->assertTrue(0, Zman::parse('March 28, 2021')->getOmerCount());
-        $this->assertTrue(1, Zman::parse('March 29, 2021')->getOmerCount());
-        $this->assertTrue(33, Zman::parse('April 30, 2021')->getOmerCount());
-        $this->assertTrue(49, Zman::parse('May 16, 2021')->getOmerCount());
-        $this->assertTrue(0, Zman::parse('May 17, 2021')->getOmerCount());
-        $this->assertTrue(0, Zman::parse('November 14, 2021')->getOmerCount());
+        $this->assertEquals(0, Zman::parse('March 28, 2021')->getOmerCount());
+        $this->assertEquals(1, Zman::parse('March 29, 2021')->getOmerCount());
+        $this->assertEquals(33, Zman::parse('April 30, 2021')->getOmerCount());
+        $this->assertEquals(49, Zman::parse('May 16, 2021')->getOmerCount());
+        $this->assertEquals(0, Zman::parse('May 17, 2021')->getOmerCount());
+        $this->assertEquals(0, Zman::parse('November 14, 2021')->getOmerCount());
     }
 }
